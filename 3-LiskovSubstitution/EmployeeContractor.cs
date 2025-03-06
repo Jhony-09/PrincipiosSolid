73f646feec014324d24805d34122ec3686fd52cd
Jhony Estuardo Mariano Choc Tiul <jhonychocpc@gmail.com>
2025-03-06 10:37:22 -0600

    refactor: agregar principio solid No.3

--- 3-LiskovSubstitution/EmployeeContractor.cs
+++ 3-LiskovSubstitution/EmployeeContractor.cs
@@ -2,8 +2,14 @@ namespace Liskov
 {
     public class EmployeeContractor : Employee
     {
-        public EmployeeContractor(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
+        public EmployeeContractor(string fullname, int hoursWorked) : base(fullname, hoursWorked)
         {
+        }
+
+        public override decimal Salary()
+        {
+            decimal hourValue = 40 ;
+            return hourValue * (HoursWorked);
         }
     }
 }
